a982c51d33bcdc1a3f2eba28b79f4389723168d4
Geo Thomas <geothomas@cet.ac.in>
2021-05-31 15:43:59 +0530

    V 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import {useState} from 'react'
 import Counter from './components/Counter';
+import Employee from './components/Employee';
 
 function App() {
   const [count,setCount]=useState(0);
@@ -12,19 +13,29 @@ function App() {
   // }
   //Spread Operator usecase
   //call as ...obj that is object name
-  let obj={
+  let value={
     title:'First_Counter',
     count
   }
+  let emp=[{name:'Geo',age:23},{name:'Appu',age:23}]
+  //Example of external data from web service use of map func
+  //<Employee key={index} name={obj.name} age={obj.age}/>  
+  //other way use with spread operator
+  //we can avoid return in map and also use () for mult exp
+
   return (
     <div>
       <Header data={data}/>
     <h1 className={'hello'}>Hello {data}</h1>
-    <Counter {...obj}/>
+    <Counter {...value}/>
     <Counter title="Second_Counter" count={count}/>
-
-
     <button onClick={()=>setCount(count+1)}>Add</button>
+    {
+      emp.map((obj,index)=>
+          <Employee key={index} {...obj}/>
+      
+      )
+    }
     </div>
   );
 }
---
+++ my-app/src/components/Employee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Employee({name,age}) {
+    return (
+        <div>
+            <h1>Name :{name}</h1>
+            <h4>Age  :{age}</h4>
+        </div>
+    )
+}
+
+export default Employee
