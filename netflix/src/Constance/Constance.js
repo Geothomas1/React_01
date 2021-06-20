6fbce77bdd76a6c56fb2d9d3c6def301111bc672
Geo Thomas <geothomas@cet.ac.in>
2021-06-20 22:48:53 +0530

    file change

--- netflix/src/Constance/Constance.js
+++ netflix/src/Constance/Constance.js
@@ -1,3 +1,3 @@
 export const baseURL='https://api.themoviedb.org/3';
-export const API_KEY='#';
+export const API_KEY='f3064474e28f8216042dfec242d36316';
 export const imageURL='https://image.tmdb.org/t/p/original';
