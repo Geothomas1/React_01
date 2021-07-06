a689e34a2eacef0e5778fa48025387c21c5fb03c
Geo Thomas <geothomas@cet.ac.in>
2021-07-06 14:39:05 +0530

    Redux

---
+++ reduxt/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import reduxDemo from './reduxDemo';
+function App() {
+
+  return (
+    <div>
+      <reduxDemo></reduxDemo>
+    </div>
+    
+  );
+}
+
+export default App;
